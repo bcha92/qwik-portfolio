75d0ad720a32a15d591d547157ef282e7896b979
bcha92@hotmail.com <bcha92@hotmail.com>
2023-11-19 00:47:07 -0500

    adjusted footer

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -2,6 +2,13 @@ import { component$ } from "@builder.io/qwik";
 import { useServerTimeLoader } from "~/routes/layout";
 import styles from "./footer.module.css";
 
+const dateOptions: Intl.DateTimeFormatOptions = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
 export default component$(() => {
   const serverTime = useServerTimeLoader();
 
@@ -9,9 +16,14 @@ export default component$(() => {
     <footer>
       <div class="container">
         <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
+          <span>Created with Qwik ♡ by Builder.io</span>
           <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
+          <span>
+            {new Date(serverTime.value.date).toLocaleDateString(
+              undefined,
+              dateOptions,
+            )}
+          </span>
         </a>
       </div>
     </footer>
