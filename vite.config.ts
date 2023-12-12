973916d71aef42d97fb5243789405a48d11b9bf8
bcha92@hotmail.com <bcha92@hotmail.com>
2023-12-12 18:39:21 -0500

    removed insights + analytics --will use checkly instead

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// Vercel Analytics and Speed Insights
-import { inject } from "@vercel/analytics";
-import { injectSpeedInsights } from "@vercel/speed-insights";
-
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), inject(), injectSpeedInsights({})],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     dev: {
       headers: {
         "Cache-Control": "public, max-age=0",
