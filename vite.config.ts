83eb04c432c095972ba9204a512102451eac174b
bcha92@hotmail.com <bcha92@hotmail.com>
2023-12-12 18:32:30 -0500

    vite config push for test

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// Vercel Analytics and Speed Insights
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), inject(), injectSpeedInsights({})],
     dev: {
       headers: {
         "Cache-Control": "public, max-age=0",
