e30ec17f1fd90d0cd49d60fd715a7ff71dfa373f
bcha92@hotmail.com <bcha92@hotmail.com>
2024-02-28 21:51:38 -0500

    new slide embed link

--- src/routes/projects/intro-qwik/index.tsx
+++ src/routes/projects/intro-qwik/index.tsx
@@ -5,12 +5,10 @@ export default component$(() => {
     <div class="flex flex-col items-center">
       <h2 class="mb-8">Introduction to QwikJS</h2>
       <iframe
-        src="https://docs.google.com/presentation/d/1WeNbesLQZACc-NKyk2DWKsWzLK5l_NZ8cPcS7v2eAjQ"
-        frameBorder="0"
+        src="https://docs.google.com/presentation/d/e/2PACX-1vS6gYHl_AWmsvuRkzoCABkJpYQj7nRAor8i0sjQm-mx5IRwAlVsrnrdhTtKUfCHn8sxdputrnVUikqR/embed?start=false&loop=false&delayms=30000"
         width="960"
         height="569"
         allowFullscreen={true}
-        class="center mb-4 flex"
       ></iframe>
     </div>
   );
