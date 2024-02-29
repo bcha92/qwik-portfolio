dc120360691bec213c462ffedc2697ddb6fa2c8c
bcha92@hotmail.com <bcha92@hotmail.com>
2024-02-28 21:47:56 -0500

    updated link

--- src/routes/projects/intro-qwik/index.tsx
+++ src/routes/projects/intro-qwik/index.tsx
@@ -5,7 +5,7 @@ export default component$(() => {
     <div class="flex flex-col items-center">
       <h2 class="mb-8">Introduction to QwikJS</h2>
       <iframe
-        src="https://docs.google.com/presentation/d/e/2PACX-1vTqfYy7UtJ-WfC3zE3MsqI-R8sV6c10Sq-EIaqKQIr7DBfLM_dWP_79YNbLFlmXv_AtP5Pg8gBpVdAB/embed?start=false&loop=true&delayms=10000"
+        src="https://docs.google.com/presentation/d/1WeNbesLQZACc-NKyk2DWKsWzLK5l_NZ8cPcS7v2eAjQ"
         frameBorder="0"
         width="960"
         height="569"
